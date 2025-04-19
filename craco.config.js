c5cdf149e746276e8a01b957be48a701186cae8c
Alba Veliu <albenita.veliu@dpschool.io>
2025-04-19 20:35:50 +0200

    for netlifu

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   babel: {
     plugins: ['@emotion']
+  },
+  webpack: {
+    configure: {
+      resolve: {
+        fallback: {
+          path: require.resolve('path-browserify'),
+          fs: false
+        }
+      }
+    }
   }
 }; 
