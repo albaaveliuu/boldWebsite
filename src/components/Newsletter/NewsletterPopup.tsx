c090125fa4266991e20877c24f39e0bb8e055e02
Alba Veliu <albenita.veliu@dpschool.io>
2025-04-21 20:45:30 +0200

    new change

--- src/components/Newsletter/NewsletterPopup.tsx
+++ src/components/Newsletter/NewsletterPopup.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { motion, domAnimation, LazyMotion } from 'framer-motion';
-import { useForm, ValidationError } from '@formspree/react';
 import headerImage from '../../images/header.png';
 
 const PopupOverlay = styled(motion.div)`
